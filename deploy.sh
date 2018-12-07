6f8f86258fab0583fa0921c7cd3e1845809b2974
Naiba <1@5.nu>
2018-12-07 09:53:07 +0800

    fix path

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 rm -rf dist &&\
     npm run build &&\
     cd dist &&\
-    ssh -t -p60030 root@ca.sx.p4x.com 'rm -rf /NAIBA/www/runcuo/frontend* && ls -al /NAIBA/www/runcuo/frontend' &&\
-    rsync -rvz -e 'ssh -p 60030' --progress --remove-sent-files . root@ca.sx.p4x.com:/NAIBA/www/runcuo/frontend
+    ssh -t -p60030 root@ca.sx.p4x.com 'rm -rf /NAIBA/www/runcuo/frontend/* && ls -al /NAIBA/www/runcuo/frontend/' &&\
+    rsync -rvz -e 'ssh -p 60030' --progress --remove-sent-files . root@ca.sx.p4x.com:/NAIBA/www/runcuo/frontend/
--- pdeplay.sh
+++ pdeplay.sh
@@ -1,5 +1,5 @@
 rm -rf dist &&\
     npm run build &&\
     cd dist &&\
-    ssh -o ProxyCommand='nc -x 127.0.0.1:1086 %h %p' -t -p60030 root@ca.sx.p4x.com 'rm -rf /NAIBA/www/runcuo/frontend* && ls -al /NAIBA/www/runcuo/frontend' &&\
-    rsync -rvz -e "ssh -p 60030 -o ProxyCommand='nc -x 127.0.0.1:1086 %h %p'" --progress --remove-sent-files . root@ca.sx.p4x.com:/NAIBA/www/runcuo/frontend
+    ssh -o ProxyCommand='nc -x 127.0.0.1:1086 %h %p' -t -p60030 root@ca.sx.p4x.com 'rm -rf /NAIBA/www/runcuo/frontend/* && ls -al /NAIBA/www/runcuo/frontend/' &&\
+    rsync -rvz -e "ssh -p 60030 -o ProxyCommand='nc -x 127.0.0.1:1086 %h %p'" --progress --remove-sent-files . root@ca.sx.p4x.com:/NAIBA/www/runcuo/frontend/
